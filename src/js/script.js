c2c70e6600f74a81488c5b995e5824882f3497d4
Ivan Moshak <ivanmoshak11@gmail.com>
2023-12-10 18:53:09 +0200

    update

--- src/js/script.js
+++ src/js/script.js
@@ -9,6 +9,8 @@ const loadMoreBtn = document.querySelector('.load-more');
 
 let page = 1;
 let searchQuery = '';
+let loadedImagesCount = 0;
+loadMoreBtn.style.display= 'none';
 
 form.addEventListener('submit', async function (event) {
   event.preventDefault();
@@ -72,16 +74,23 @@ function handleResponse(data) {
 
   if (hits.length === 0) {
     Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+    loadMoreBtn.style.display= 'none';
     return;
+  } else {
+    Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+    gallery.insertAdjacentHTML('beforeend', createImageCard(hits));
+    lightbox.refresh();
+    smoothScrollToGallery();
+  };
+
+  loadedImagesCount += hits.length;
+
+  if (loadedImagesCount >= totalHits) {
+    Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+    loadMoreBtn.style.display = 'none';
+  } else {
+    loadMoreBtn.style.display = 'inline';
   }
-
-  Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
-
-  gallery.insertAdjacentHTML('beforeend', createImageCard(hits));
-
-  lightbox.refresh();
-
-  smoothScrollToGallery();
 };
 
 function createImageCard(hit) {
